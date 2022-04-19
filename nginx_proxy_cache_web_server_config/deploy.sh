fe216e432efafcba58911d58cf0a69d4b85bd376
rawdata123 <amir.rawdat@hotmail.com>
2022-04-19 16:40:44 -0700

    Added Step 3 for caching and dashboard

--- nginx_proxy_cache_web_server_config/deploy.sh
+++ nginx_proxy_cache_web_server_config/deploy.sh
@@ -10,9 +10,12 @@ fi
 #   exit 1;
 # fi
 
-mkdir -p /tmp/nginx/cache
-sudo cp step_2/html/main.html /usr/share/nginx/html
-sudo cp step_2/images/Grand-Canyon.jpg /usr/share/nginx
+if [ $1 = 2 ]; then
+  sudo cp step_2/html/main.html /usr/share/nginx/html
+  sudo cp step_2/images/Grand-Canyon.jpg /usr/share/nginx
+elif [ $1 = 3 ]; then
+  mkdir -p /tmp/nginx/cache
+fi
 
 sudo cp -r step_$1/* /etc/nginx
 sudo nginx -s reload

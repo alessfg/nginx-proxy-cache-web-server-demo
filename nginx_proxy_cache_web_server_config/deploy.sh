5f139c53e76a2456eb287073ecb036a03610af92
Alessandro Fael Garcia <alessfg@hotmail.com>
2022-04-20 17:13:43 +0200

    Multiple fixes
    * Fix step 3
    * Check the NGINX config is valid before reloading
    * Refactor config files to improve consistency

--- nginx_proxy_cache_web_server_config/deploy.sh
+++ nginx_proxy_cache_web_server_config/deploy.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  echo "You need to pass a step number"
+  echo "You need to pass a step number (1 - 3)"
   exit 1;
 fi
 
-# if (($1 < 1 || $1 > 8)); then
-#   echo "Only steps 1 through 8 exist"
-#   exit 1;
-# fi
-
-if [ $1 = 2 ]; then
-  sudo cp step_2/html/main.html /usr/share/nginx/html
-  sudo cp step_2/images/Grand-Canyon.jpg /usr/share/nginx
-elif [ $1 = 3 ]; then
-  mkdir -p /tmp/nginx/cache
+if (( $1 < 1 || $1 > 3 )); then
+  echo "Only steps 1 through 3 exist!"
+  exit 1;
 fi
 
-sudo cp -r step_$1/* /etc/nginx
-sudo nginx -s reload
+if [ $1 = 2 ]; then
+  sudo cp -r step_2/html /usr/share/nginx
+  sudo cp -r step_2/images /usr/share/nginx
+elif [ $1 = 3 ]; then
+  sudo mkdir -p /tmp/nginx/cache
+fi
 
+sudo cp -r step_$1/conf.d/* /etc/nginx/conf.d
+
+sudo nginx -t
+sudo nginx -s reload
 if [ $? = 0 ]; then
   echo "NGINX successfully reloaded"
 fi
